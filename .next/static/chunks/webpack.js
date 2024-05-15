1b299a30d8c0ea4179ee0a683f8c60d6d9224fd3
MARK <markza9822@gmail.com>
2024-05-15 09:53:01 +0700

    push v1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd0d2480920c50db"; }
+/******/ 		__webpack_require__.h = function() { return "6fb4e340c87669ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
