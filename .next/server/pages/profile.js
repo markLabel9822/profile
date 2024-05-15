cf6027d42dd8f12eb611806b641df843a250b874
MARK <markza9822@gmail.com>
2024-05-15 09:47:48 +0700

    create project

---
+++ .next/server/pages/profile.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/profile";
+exports.ids = ["pages/profile"];
+exports.modules = {
+
+/***/ "./src/pages/profile/index.tsx":
+/*!*************************************!*\
+  !*** ./src/pages/profile/index.tsx ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst profile = ()=>{\n    return 123;\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (profile);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvcHJvZmlsZS9pbmRleC50c3guanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBLEtBQUssQ0FBQ0EsT0FBTyxPQUFTLENBQUM7SUFDckIsTUFBTSxDQUFDLEdBQUc7QUFDWixDQUFDO0FBRUQsaUVBQWVBLE9BQU8sRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL25vdGVzLy4vc3JjL3BhZ2VzL3Byb2ZpbGUvaW5kZXgudHN4P2Q1NmQiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgcHJvZmlsZSA9ICgpID0+IHtcbiAgcmV0dXJuIDEyMztcbn07XG5cbmV4cG9ydCBkZWZhdWx0IHByb2ZpbGU7XG4iXSwibmFtZXMiOlsicHJvZmlsZSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/pages/profile/index.tsx\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./src/pages/profile/index.tsx"));
+module.exports = __webpack_exports__;
+
+})();
